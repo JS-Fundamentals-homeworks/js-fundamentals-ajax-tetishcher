0efefc5571ee79edf44faf2ee77623fe0176e344
Tetiana Kharkhalis <tetianascherbata@MacBook-Air-Tetiana.local>
2025-02-27 15:44:57 +0200

    Homework

--- task1.js
+++ task1.js
@@ -4,4 +4,22 @@
 // Імена користувачів відобразити в ненумерованому списку ul.usersList,
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
-// Перевірити за допомогою команди npm tests/task1.test.js 
+// Перевірити за допомогою команди npm tests/task1.test.js
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Response error");
+        }
+        return response.json();
+    })
+    .then(users => {
+        const usersList = document.querySelector(".usersList");
+        users.forEach(user => {
+            const listItem = document.createElement("li");
+            listItem.textContent = user.name;
+            usersList.appendChild(listItem);
+        });
+    })
+    .catch(error => {
+        console.error("Fetching users' error:", error);
+    });
--- task2.js
+++ task2.js
@@ -5,3 +5,28 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+document.getElementById("getUserButton").addEventListener("click", function() {
+    const userName = document.getElementById("userNameInput").value.trim();
+    const url = "https://jsonplaceholder.typicode.com/users";
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Response error");
+            }
+            return response.json();
+        })
+        .then(users => {
+            const user = users.find(u => u.name.toLowerCase() === userName.toLowerCase());
+
+            const userCityElement = document.getElementById("userCity");
+            if (user) {
+                userCityElement.textContent = `City: ${user.address.city}`;
+            } else {
+                userCityElement.textContent = "User not found";
+            }
+        })
+        .catch(error => {
+            console.error("There was a problem with the fetch operation:", error);
+        });
+});
